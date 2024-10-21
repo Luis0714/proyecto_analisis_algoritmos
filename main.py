7274f25fa762c5e578ed50bb1d789258a090894e
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2024-10-20 21:50:36 -0500

    Refactor main.py and add functionality to calculate probabilities

--- main.py
+++ main.py
@@ -15,11 +15,13 @@ def main():
     print("Matriz original")
     print(matriz,'\n')
     
+    #----------------------------------------------------------------------------------------------------------------
     #prubas con sistema candidato con tres variables ABC
     # estado_inicial = [0, 0, 0, 0]
     # variables_sistema_candidato = [1, 1, 1, 0] # 1 indica que se tiene en cuenta la variable, 0 que no se tiene en cuenta A, B, C menos D
     # matriz_sistema_candidato = generador_matriz.generar_matriz_sistema_candidato(matriz, estado_inicial, variables_sistema_candidato)
 
+    #****************************************************************************************************************
     #prueba con todas las variables del estado actual y futuro
     # variable_estado_futuro = [1, 1, 1] # ABC t+1
     # variable_estado_actual = [1, 1, 1] # ABC t
@@ -30,8 +32,11 @@ def main():
     #                             variable_estado_futuro,
     #                             variable_estado_actual,
     #                             estado_inicial)
+    #****************************************************************************************************************
+
 
     
+    #****************************************************************************************************************
     #prueba con estado actual completo y estado futuro con un elemento
     # variable_estado_futuro = [0,1,0] # B t+1
     # variable_estado_actual = [1, 1, 1] # ABC t
@@ -40,7 +45,10 @@ def main():
     #                                 variable_estado_futuro,
     #                                 variable_estado_actual,
     #                                 estado_inicial)
+    #****************************************************************************************************************
 
+
+    #****************************************************************************************************************
     #prueba con estado actual imcompleto y estado futuro imcompleto
     # variable_estado_futuro = [0,1,0] # B t+1
     # variable_estado_actual = [1, 1, 0] # AB t
@@ -50,7 +58,9 @@ def main():
     #                                 variable_estado_futuro,
     #                                 variable_estado_actual,
     #                                 estado_inicial)
+    #****************************************************************************************************************
 
+    #****************************************************************************************************************
     #pruba para que se generen subproblemas
     # variable_estado_futuro = [1,1,0] # AB t+1
     # variable_estado_actual = [1, 1, 0] # AB t
@@ -59,15 +69,18 @@ def main():
     #                                 variable_estado_futuro,
     #                                 variable_estado_actual,
     #                                 estado_inicial)
+    #****************************************************************************************************************
 
 
+    #----------------------------------------------------------------------------------------------------------------
     #pruebas con sistema candidato con dos variables
-    estado_inicial = [0, 1, 0, 1] # para aplicar en matriz original = A = 0, B = 0, C = 0, D = 0
+    estado_inicial = [0, 1, 0, 1] # para aplicar en matriz original = A = 1, B = 1, C = 0, D = 1
     variables_sistema_candidato = [1, 0, 0, 1] # solo tener en cuenta A y D
     matriz_sistema_candidato = generador_matriz.generar_matriz_sistema_candidato(matriz, estado_inicial, variables_sistema_candidato)
     print("Matriz sistema candidato")
     print(matriz_sistema_candidato, '\n')
 
+    #****************************************************************************************************************
     #prueba con todas las variables del estado actual y futuro
     # variable_estado_futuro = [1, 1] # AD t+1
     # variable_estado_actual = [1, 1] # AD t
@@ -76,7 +89,12 @@ def main():
     #                             variable_estado_futuro,
     #                             variable_estado_actual,
     #                             estado_inicial)
+
+    #****************************************************************************************************************
     
+
+
+    #****************************************************************************************************************
     #prueba con estado actual completo y estado futuro con un elemento
     # variable_estado_futuro = [0, 1] # D t+1
     # variable_estado_actual = [1, 1] # AD t
@@ -85,7 +103,12 @@ def main():
     #                             variable_estado_futuro,
     #                             variable_estado_actual,
     #                             estado_inicial)
+    #****************************************************************************************************************
 
+
+
+
+    #****************************************************************************************************************
     #prueba que se generen subproblemas
     variable_estado_futuro = [1, 1] # AD t+1
     variable_estado_actual = [1, 0] # A t
@@ -94,6 +117,7 @@ def main():
                                 variable_estado_futuro,
                                 variable_estado_actual,
                                 estado_inicial)
+    #****************************************************************************************************************
 
 
    
